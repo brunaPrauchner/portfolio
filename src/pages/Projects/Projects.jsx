2fcc9900ff70fa2bc78a0ae7639dbc32b521d108
brunaPrauchner <bruna.prauchner@acad.pucrs.br>
2021-07-04 17:24:23 -0300

    :art: structure improved for page projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,19 +1,19 @@
-import { TEXT } from '../../constants/constants';
-import Project from '../../components/Project/Project';
-import GinaTells from '../../assets/images/gina1c.png';
-import SaveG from '../../assets/images/gira1.png';
-import Weat from '../../assets/images/weat1.png';
-import './Projects.css';
+import { TEXT } from '../../constants/constants'
+import Project from '../../components/Project/Project'
+import GinaTells from '../../assets/images/gina1c.png'
+import SaveG from '../../assets/images/gira1.png'
+import Weat from '../../assets/images/weat1.png'
 
 function Projects() {
   return (
     <div>
-     <h3 className='title-proj'>Projects</h3>
-      <ul>
-        <Project title='Gina Tells' desc={TEXT.P1} img={GinaTells} />
-        <Project title='Save Giraffe' desc={TEXT.P2} img={SaveG} />
-        <Project title='Check Weather' desc={TEXT.P3} img={Weat} />
-      </ul>
+      <h3>Projects</h3>
+      <Project title='Gina Tells' link='https://www.w3schools.com' desc={TEXT.P1} img={GinaTells} />
+      <Project title='Save Giraffe' link='https://github.com/brunaPrauchner/AI-AutomatedPlanning-SaveGiraffe' desc={TEXT.P2} img={SaveG} />
+      <Project title='Find your GIPHY' link='https://www.w3schools.com' desc={TEXT.P3} img={Weat} />
+      <Project title='Fox Adm Tool' link='https://github.com/brunaPrauchner/fox-adm-tool-api' desc={TEXT.P3} img={Weat} />
+      <Project title='Check Weather' link='https://www.w3schools.com' desc={TEXT.P3} img={Weat} />
+      <Project title='KPI Tracking' link='https://www.w3schools.com' desc={TEXT.P3} img={Weat} />
     </div>
   );
 }
