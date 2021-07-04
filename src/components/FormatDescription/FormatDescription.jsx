d5605fc9b71682f84ee09f8d4c3bc719e61fc7e9
brunaPrauchner <bruna.prauchner@acad.pucrs.br>
2021-07-03 21:41:32 -0300

    :art: page about improved

---
+++ src/components/FormatDescription/FormatDescription.jsx
@@ -0,0 +1,32 @@
+import './FormatDescription.css';
+
+const FormatDescription = ({ desc, img, size }) => {
+  const style = {}
+
+  switch (size) {
+    case 'small':
+      style.width = '200px'
+      style.height = '200px'
+      break;
+
+    case 'medium':
+      style.width = '350px'
+      style.height = '350px'
+      break;
+
+    case 'large':
+      style.width = '100px'
+      style.height = '100px'
+      break;
+  }
+
+  return (
+    <div className='container-format'>
+      <p className='item-desc'>{desc}</p>
+      <div className='item-img'>
+        <img src={img} style={style} />
+      </div>
+    </div>
+  )
+}
+export default FormatDescription;
